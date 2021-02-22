63724e8ee8570d8f77859509a59b9338625ce039
fabricio <luminato.silva@gmail.com>
2021-02-22 09:49:06 -0300

    add packge -  get: ^3.25.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:gamesapi/pages/home/home_page.dart';
+import 'package:get/get.dart';
+
+import './pages/home/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +10,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
